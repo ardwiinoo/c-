ea0ce39d121ca7186140846df4bfb49ee7b37e7b
Arif Dwi Nugroho <78846749+ardwiinoo@users.noreply.github.com>
2021-02-10 20:21:29 +0700

    Add files via upload

---
+++ 00_printing/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+//single command
+/*multi
+line
+command*/
+
+int main()
+{
+    std::cout << "Mulai aja dulu\n";
+    std::cout << "apapun itu" << std::endl;
+    std::cin.get();
+    return 0;
+}
