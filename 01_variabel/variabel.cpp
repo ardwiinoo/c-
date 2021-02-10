dfa8af93ce9385af6e5403b9af684f0ac857e45f
Arif Dwi Nugroho <78846749+ardwiinoo@users.noreply.github.com>
2021-02-10 20:22:56 +0700

    Add files via upload

---
+++ 01_variabel/variabel.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    i = 50;
+    cout << i << endl;
+
+    int j;
+    cout << "masukkan nilai: ";
+    cin >> j;
+    cout << "nilai yang anda masukkan: ";
+    cout << j << endl;
+
+    cin.get();
+    return 0;
+}
