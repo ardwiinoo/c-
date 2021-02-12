3a498572291079dcff682761d97313d4bcf7ca34
Arif Dwi Nugroho <78846749+ardwiinoo@users.noreply.github.com>
2021-02-12 08:26:22 +0700

    Add files via upload

---
+++ 11_while/while.cpp
@@ -0,0 +1,25 @@
+  #include <iostream>
+  
+  using namespace std;
+  
+  int main(){
+  	
+  	int a = 7;
+  	
+  	//while looping
+  	/* 
+		while( syarat ){
+		
+		aksi
+		
+		}
+	*/
+	
+  	while(a < 15){
+  		cout << "hallo kalian !";
+  		cout << a << endl;
+  		a += 2;
+	  }
+	  
+	  return 0;
+  }
