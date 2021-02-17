a315c19c82db320545dbfd986a0a11ca425043e1
Arif Dwi Nugroho <78846749+ardwiinoo@users.noreply.github.com>
2021-02-17 18:05:42 +0700

    Add files via upload

---
+++ 32_string/string.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    //std::string namaVar("kata");
+
+    string kata("Halo Kalian");
+    cout << kata << endl;
+
+    return 0;
+}
